c7c0a3262cfc2ca6e5f5afcc7f9e95ca9d921331
Farzona <fpallaeva@gmail.com>
2015-11-12 17:23:33 +0600

    infinite loop while

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -4,14 +4,11 @@
 def summa(a, b):
     return a + b 
 
-
 def deduction(a, b):
     return a - b
 
-
 def multiplication(a, b):
     return a * b 
-
 
 def division(a, b):
     result = None
@@ -20,48 +17,58 @@ def division(a, b):
         result = 'you tried to divide by zero'
     else:
         result = a / b
-
     return result
 
-print("Enter x : ")
-x = input()
-x = int(x)
+choice = None
+result = None
 
-print("Enter y : ")
-y = input()
-y = int(y)
+while choice != '0':
+    print("Enter x : ")
+    x = input()
+    x = int(x)
 
-print("x = " + str(x))
-print("y = " + str(y))
+    print("Enter y : ")
+    y = input()
+    y = int(y)
+
+    print("Choose operator: ")
+    print("0) exit")
+    print("1) +")
+    print("2) -")
+    print("3) *")
+    print("4) /")
+    choice = input()
+
+    operator = None
 
 
-print("Choose operator: ")
-print("1) +")
-print("2) -")
-print("3) *")
-print("4) /")
-choice = input()
+    ##
+    #   Using functions
+    ##
+    if choice == '1' or choice == '+':
+        operator = '+'
+        result = summa(x, y) 
 
-operator = None
+    if choice == '2' or choice == '-':
+        operator = '-'
+        result = deduction(x, y)
 
-##
-#   Using functions
-##
+    if choice == '3' or choice == '*':
+        operator = '*'
+        result = multiplication(x, y)
 
-if choice == '1' or choice == '+':
-    operator = '+'
-    result = summa(x, y) 
+    if choice == '4' or choice == '/':
+        operator = '/'
+        result = division(x, y)
 
-if choice == '2' or choice == '-':
-    operator = '-'
-    result = deduction(x, y)
+    if operator == None or result == None:
+        print('Are you sure? [Yes|No]')
+        sure = input() # sure => 'Y'
+        sure = sure.lower() # sure => 'y'
 
-if choice == '3' or choice == '*':
-    operator = '*'
-    result = multiplication(x, y)
-
-if choice == '4' or choice == '/':
-    operator = '/'
-    result = division(x, y)
-
-print("x " + operator + " y = " + str(result))
+        if sure == 'y' or sure == 'yes': # 'y' != 'Y'
+            print('Bye!!')
+        elif sure == 'n' or sure == 'no':
+            choice = None 
+    else:
+        print("x " + str(operator) + " y = " + str(result))

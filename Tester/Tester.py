1dadab3a1b78f1ca96934d4dd2d5fa3212111725
Farzona <fpallaeva@gmail.com>
2016-01-26 11:39:17 +0600

    fix #16

--- Tester/Tester.py
+++ Tester/Tester.py
@@ -14,7 +14,23 @@ def check():
         question1 = request.form.get('question1')
         if question1 == '1':
             points = points + 1
+        
+        question2 = request.form.get('question2')
+        if question2 == '1':
+            points = points + 1
 
+        question3 = request.form.get('question3')
+        if question3 == '1':
+            points = points + 1
+        
+        question4 = request.form.get('question4')
+        if question4 == '1':
+            points = points + 1
+
+        question5 = request.form.get('question5')
+        if question5 == '2':
+            points = points + 1
+    
     return render_template("check.html", result=points)
    
 if __name__ == "__main__":

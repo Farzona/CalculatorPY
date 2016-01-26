cf301d47aed8f929cf6579ccdbed7f2f779bcb09
Farzona <fpallaeva@gmail.com>
2016-01-26 10:24:01 +0600

    fix #15

--- Tester/Tester.py
+++ Tester/Tester.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__,)
   
 @app.route("/")
@@ -8,9 +8,15 @@ def index():
 
 @app.route("/check", methods=['POST'])
 def check():
-    return render_template("check.html")
+    points = 0
 
+    if request.form != None:
+        question1 = request.form.get('question1')
+        if question1 == '1':
+            points = points + 1
 
+    return render_template("check.html", result=points)
+   
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
